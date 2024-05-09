d600e859967924055d22e8782afbbf680bbd03e0
scottskim <scottskim30@gmail.com>
2024-05-09 14:49:03 -0400

    added dataset storage and conversion to npz, brought back non-spherical gaussians

--- gaussian_splatting/trainer.py
+++ gaussian_splatting/trainer.py
@@ -136,7 +136,7 @@ class Trainer(object):
         accelerator = self.accelerator
         device = accelerator.device
         print(device)
-        
+        print(accelerator.is_main_process)
         with tqdm(initial = self.step, total = self.train_num_steps, disable = not accelerator.is_main_process) as pbar:
 
             while self.step < self.train_num_steps:
